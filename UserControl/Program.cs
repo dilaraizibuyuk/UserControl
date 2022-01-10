1a52c2c808533db5418b512e0c61c3eefbc16852
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-10 17:44:05 +0300

    Proje dosyası ekle.

---
+++ UserControl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserrControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserControl uye = new UserControl();
+            uye.KullaniciAdi = "software";
+            uye.Sifre = "12345";
+            Console.WriteLine("Kullanıcı Adı: {0}"+"\n"+"Şifre:{1}",uye.KullaniciAdi,uye.Sifre);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserControl/UserControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserrControl
+{
+    class UserControl
+    {
+        private string _kullaniciadi;
+        private string _sifre;
+
+        public string KullaniciAdi
+        {
+            get
+            {
+                return _kullaniciadi;
+            }
+
+            set
+            {
+                bool isOK = true;
+                for (int i = 0; i < value.Length; i++)
+                {
+                     if(!char.IsLetter(value[i])) 
+                    {
+                        isOK = false;
+                        break;
+                    }
+
+                }
+
+                if (isOK)
+                {
+                    _kullaniciadi = value;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı Adı Harf Dışında Karakter İçeremez. Lütfen Düzeltin..");
+                }
+
+
+            }
+        }
+        public string Sifre
+        {
+            get
+            {
+                return _sifre;
+            }
+
+            set
+            {
+                _sifre = value;
+            }
+        }
+    }
+}
